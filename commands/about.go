0a88294ca9ec4ec3c356cee65d7fb118c50f20e5
Tch1b0 <Jpour3006@gmail.com>
2021-05-26 13:25:59 +0200

    about: added description

--- commands/about.go
+++ commands/about.go
@@ -1,11 +1,15 @@
 package commands
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"github.com/bwmarrin/discordgo"
+)
 
 func About(s *discordgo.Session, m *discordgo.MessageCreate) {
 	e := discordgo.MessageEmbed{}
 	e.Title = "About"
-	e.Description = ""
+	e.Description = (
+		"My developer is **Tch1b0**\n and you can find my **sourcecode** on GitHub:\n"+
+		"https://github.com/Tch1b0/MoGo")			
 	s.ChannelMessageSendEmbed(
 		m.ChannelID,
 		&e,
