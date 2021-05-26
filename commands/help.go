42a3128dd6725f03ab18b666d0a678f3581c0e0a
Tch1b0 <Jpour3006@gmail.com>
2021-05-26 10:01:27 +0200

    help: added the help command

--- commands/help.go
+++ commands/help.go
@@ -1,7 +1,22 @@
-package main
+package commands
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"fmt"
 
-func help(s *discordgo.Session, m *discordgo.MessageCreate){
-	s.ChannelMessageSend(m.ChannelID, "https://github.com/Tch1b0/MoGo")
+	"github.com/bwmarrin/discordgo"
+)
+
+func Help(s *discordgo.Session, m *discordgo.MessageCreate){
+	answer := fmt.Sprintf(
+		"Hi! I am %s and you know what I don't like? Too long links.\n"+
+		"But that is what I was made for!\n"+
+		"I am here to **automatically** shorten your links!\n\n"+
+		"Here you can get furhter information how I was made:\n"+
+		"https://github.com/Tch1b0/MoGo", 
+		s.State.User.Username,
+	)
+	s.ChannelMessageSend(
+		m.ChannelID, 
+		answer,
+	)
 }
